23bc955c333da2919b93e885082e950c55b617ea
Leo <leo.cho1989@gmail.com>
2021-07-17 19:51:25 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const db = require('./config/keys').mongoURI;
+const passport = require('passport'); 
+const path = require('path');
+
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
